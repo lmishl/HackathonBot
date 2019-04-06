69a4a83b108777d7922319d7b010919f45f4b71e
CliffordSimak <holamellamosasha@gmail.com>
2019-04-06 14:59:53 +0300

    Update MovementChecker.cs

--- FirstBot/Model/MovementChecker.cs
+++ FirstBot/Model/MovementChecker.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using IO.Swagger.Model;
 using static IO.Swagger.Model.ValueTupleLocationSurfaceType;
@@ -6,23 +7,39 @@ namespace FirstBot.Model
 {
     public class MovementChecker
     {
-        public bool CanMove (DirectionEnum direction, Location currentLocation, PlayerSessionInfo playerSession )
+        public bool CanMove (DirectionEnum direction, Location currentLocation, List<ValueTupleLocationSurfaceType> neighbourCells, int acceleration)
         {
-            var newLocation = currentLocation.Plus(Constants.Deltas[direction]);
+            var newCell = GetNewCell (direction, currentLocation, neighbourCells);
 
-            var newCell = playerSession.NeighbourCells.First(cell => cell.Item1 == newLocation);
-
-            if (!newCell.Item2.HasValue)
-            {
-                return false;
-            }
-
-            if (newCell.Item2.Value == Item2Enum.Rock )
+            // Горы
+            if (newCell.Item2.HasValue && newCell.Item2.Value == Item2Enum.Rock)
             {
                 return false;
             }
 
             return true;
+        }
+
+        public bool isDangerousArea (DirectionEnum direction, Location currentLocation, List<ValueTupleLocationSurfaceType> neighbourCells)
+        {
+            var newCell = GetNewCell (direction, currentLocation, neighbourCells);
+
+            // Опасная зона
+            if (newCell.Item2.HasValue && newCell.Item2.Value == Item2Enum.DangerousArea)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private ValueTupleLocationSurfaceType GetNewCell (DirectionEnum direction, Location currentLocation, List<ValueTupleLocationSurfaceType> neighbourCells)
+        {
+            var newLocation = currentLocation.Plus (Constants.Deltas[direction]);
+
+            var newCell = neighbourCells.First (cell => cell.Item1 == newLocation);
+
+            return newCell;
         }
     }
 }
