525f03df635f5c171c899f0024b49380ef2ca993
Mikhail <m.laptev93@gmail.com>
2019-04-06 12:13:16 +0300

    server added

--- FirstBot/GameService.cs
+++ FirstBot/GameService.cs
@@ -12,7 +12,7 @@ namespace FirstBot
     {
         private HttpClient client {get; set;}
 
-        private const string ServerName = "http://ServerName";
+        private const string ServerName = "http://51.15.100.12:5000";
 
         private const string Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFcnpoYW5HZXRVcCIsImp0aSI6IjRkMWY4ODkxLWNkZmEtNDBlOS05N2ZmLWMxMmYwNWRhZmU4MyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiYWJjNTJhMWEtNDQ3Yi00ZWFjLTg4NzQtMmZhOWRmOGFlMTM0Iiwicm9sIjoiYXBpX2FjY2VzcyIsImV4cCI6MTU1NzEzMTQ3NSwiaXNzIjoibXNrZG90bmV0IiwiYXVkIjoibXNrZG90bmV0In0.gorYhnqPilzcAK0xjbbXhAWKQcfxgx8UM04orx1jadk";
 
