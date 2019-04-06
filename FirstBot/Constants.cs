7d6caa9bcdc0eb611010beb4f2e33e0341fb6e20
CliffordSimak <holamellamosasha@gmail.com>
2019-04-06 13:45:44 +0300

    fix compass

--- FirstBot/Constants.cs
+++ FirstBot/Constants.cs
@@ -27,7 +27,7 @@ namespace FirstBot
         public static string JWT_Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFcnpoYW5HZXRVcCIsImp0aSI6IjRkMWY4ODkxLWNkZmEtNDBlOS05N2ZmLWMxMmYwNWRhZmU4MyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiYWJjNTJhMWEtNDQ3Yi00ZWFjLTg4NzQtMmZhOWRmOGFlMTM0Iiwicm9sIjoiYXBpX2FjY2VzcyIsImV4cCI6MTU1NzEzMTQ3NSwiaXNzIjoibXNrZG90bmV0IiwiYXVkIjoibXNrZG90bmV0In0.gorYhnqPilzcAK0xjbbXhAWKQcfxgx8UM04orx1jadk";
 
 
-        public static Dictionary<DirectionEnum, Location> Deltas;
+        public static Dictionary<DirectionEnum, Location> Deltas {get; set;}
 
     }
 }
--- FirstBot/Сompass.cs
+++ FirstBot/Сompass.cs
@@ -20,7 +20,6 @@ namespace FirstBot
             }
 
             return dict;
-            
         }
 
 
