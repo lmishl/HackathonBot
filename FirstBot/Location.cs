f6a167518676fd4d354cf8f4ed796cb267c2e23c
Mikhail <m.laptev93@gmail.com>
2019-04-06 11:27:39 +0300

    start

---
+++ FirstBot/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBot
+{
+    public class Location
+    {
+
+        public int X;
+        public int Y;
+        public int Z;
+    }
+}
---
+++ FirstBot/TurnResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstBot
+{
+    public class TurnModel
+    {
+        public string Direction;
+        public int Acceleration;
+    }
+}
