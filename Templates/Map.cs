6638261c3466b64fe7fe12ee0c69f0161ab09ad0
Сергей <butaevserg@yandex.ru>
2019-04-06 14:02:50 +0300

    Updated Map generation

--- Templates/Map.cs
+++ Templates/Map.cs
@@ -8,22 +8,42 @@ namespace Templates
 {
     public class Map
     {
-        private readonly byte _radius;
         private readonly Dictionary<HexCoordinates, HexType> _allCells;
 
-        public Map(byte radius, bool onAngle)
+        public Map(byte radius)
         {
             int total = 3 * radius * (radius - 1) + 1;
             _allCells = new Dictionary<HexCoordinates, HexType>(total);
             int start = radius - 1;
-            // fill all cells with Undiscovered value       x the most values
-            for (int x = -start; x <= start; x++)
+            for (int stp = 0; stp <= radius; stp++)
             {
-                for (int y = 0; y <= start; y++)
+                for (int y = start; y > -stp; y--)
                 {
-                    
+                    for (int x = -y; x < radius; x++)
+                    {
+                        int z = -(x + y);
+                        if (Math.Abs(x) <= start && Math.Abs(y) <= start && Math.Abs(z) <= start)
+                        {
+                            List<HexCoordinates> tempCoords = new List<HexCoordinates>(radius * 2);
+                            tempCoords.Add(new HexCoordinates(Convert.ToInt16(x), Convert.ToInt16(y), Convert.ToInt16(z)));
+                            tempCoords.Add(new HexCoordinates(Convert.ToInt16(y), Convert.ToInt16(x), Convert.ToInt16(z)));
+                            tempCoords.Add(new HexCoordinates(Convert.ToInt16(x), Convert.ToInt16(z), Convert.ToInt16(y)));
+                            tempCoords.Add(new HexCoordinates(Convert.ToInt16(z), Convert.ToInt16(x), Convert.ToInt16(y)));
+                            tempCoords.Add(new HexCoordinates(Convert.ToInt16(z), Convert.ToInt16(y), Convert.ToInt16(x)));
+                            tempCoords.Add(new HexCoordinates(Convert.ToInt16(y), Convert.ToInt16(z), Convert.ToInt16(x)));
+
+                            foreach (var coord in tempCoords)
+                            {
+                                if (!_allCells.ContainsKey(coord))
+                                    _allCells.Add(coord, HexType.Undiscovered);
+                            }
+                        }                   
+                    }
                 }
             }
+
+            if (_allCells.Count != total)
+                new Exception("Invalid amount of hexagon cells");
 
         }
     }
